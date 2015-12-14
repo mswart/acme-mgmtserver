9aeddf2e30ade77b484dfd1e65c3ce8eeaa05726
Malte Swart <mswart@devtation.de>
2015-12-14 03:27:29 +0100

    travis: rabbitmq-server lokes new enough

--- tests/scripts/setup-dependencies.sh
+++ tests/scripts/setup-dependencies.sh
@@ -3,12 +3,10 @@ set -x
 # add ppa for mariadb and rabbitmq (needs newer versions)
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 echo 'deb http://mirror2.hs-esslingen.de/mariadb/repo/10.0/ubuntu trusty main' > /etc/apt/sources.list.d/mariadb.list
-wget https://www.rabbitmq.com/rabbitmq-signing-key-public.asc -O - | apt-key add -
-echo 'deb http://www.rabbitmq.com/debian/ testing main' > /etc/apt/sources.list.d/rabbitmq.list
 # update package list
 apt-get update
 # install packages
-apt-get install -yV libltdl-dev mariadb-server mariadb-server-10.0 rabbitmq-server python2.7
+apt-get install -yV libltdl-dev mariadb-server mariadb-server-10.0 python2.7
 # configure mariadb
 # todo
 mysql -u root <<EOF
