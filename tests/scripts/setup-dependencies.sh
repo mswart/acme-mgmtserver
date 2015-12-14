4f63ed03217ac5d664ed233d5143c7f1109ab3c3
Malte Swart <mswart@devtation.de>
2015-12-14 03:07:47 +0100

    travis: fix sources.list syntax

--- tests/scripts/setup-dependencies.sh
+++ tests/scripts/setup-dependencies.sh
@@ -2,9 +2,9 @@
 set -x
 # add ppa for mariadb and rabbitmq (needs newer versions)
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-echo "'deb [arch=amd64,i386] http://mirror2.hs-esslingen.de/mariadb/repo/10.0/ubuntu trusty main'" > /etc/apt/sources.list.d/mariadb.list
+echo 'deb http://mirror2.hs-esslingen.de/mariadb/repo/10.0/ubuntu trusty main' > /etc/apt/sources.list.d/mariadb.list
 wget https://www.rabbitmq.com/rabbitmq-signing-key-public.asc -O - | apt-key add -
-echo "deb http://www.rabbitmq.com/debian/ testing main" > /etc/apt/sources.list.d/rabbitmq.list
+echo 'deb http://www.rabbitmq.com/debian/ testing main' > /etc/apt/sources.list.d/rabbitmq.list
 # update package list
 apt-get update -qq
 # install packages
