94d375dba08a0b86013beca9640f28f829e5de8b
Malte Swart <mswart@devtation.de>
2015-12-14 03:04:52 +0100

    travis: start installing boulder

---
+++ tests/scripts/setup-boulder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sed -x
+export GOPATH=~/build/go
+export PATH=$PATH:$GOPATH/bin
+mkdir ~/build/letsencrypt
+git clone git://github.com/letsencrypt/boulder.git --branch release-2015-12-07
+mkdir -p $GOPATH/src/github.com/letsencrypt/
+ln -s ~/build/letsencrypt/boulder $GOPATH/src/github.com/letsencrypt/boulder
+sed -i -e 's/root@tcp/boulder:test@tcp/' policy/_db/dbconf.yml sa/_db/dbconf.yml
+sed -i -e 's/-u root/-u boulder -ptest/' test/create_db.sh
+./test/setup.sh
+nohup python2.7 start.py &
