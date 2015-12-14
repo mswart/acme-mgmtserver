87bc28b0bf4fee6b0d8edf8a6c187238bb2ebb5e
Malte Swart <mswart@devtation.de>
2015-12-14 03:28:09 +0100

    travis: we operate within boulder dir

--- tests/scripts/setup-boulder.sh
+++ tests/scripts/setup-boulder.sh
@@ -8,6 +8,7 @@ mkdir ~/build/letsencrypt
 git clone git://github.com/letsencrypt/boulder.git --branch release-2015-12-07
 mkdir -p $GOPATH/src/github.com/letsencrypt/
 ln -s ~/build/letsencrypt/boulder $GOPATH/src/github.com/letsencrypt/boulder
+cd ~/build/letsencrypt/boulder
 sed -i -e 's/root@tcp/boulder:test@tcp/' policy/_db/dbconf.yml sa/_db/dbconf.yml
 sed -i -e 's/-u root/-u boulder -ptest/' test/create_db.sh
 ./test/setup.sh
