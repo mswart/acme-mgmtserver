a565cf46afe944548e7f97385f3276a4dae575a1
Malte Swart <mswart@devtation.de>
2015-12-14 03:52:52 +0100

    travis: debug boulder not stargin

--- tests/scripts/setup-boulder.sh
+++ tests/scripts/setup-boulder.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -x
-env
 eval "$(gimme 1.5)"
 export GOPATH=~/build/go
 export PATH=$PATH:$GOPATH/bin
@@ -10,7 +9,7 @@ cd $GOPATH/src/github.com/letsencrypt/boulder
 sed -i -e 's/root@tcp/boulder:test@tcp/' policy/_db/dbconf.yml sa/_db/dbconf.yml
 sed -i -e 's/-u root/-u boulder -ptest/' test/create_db.sh
 ./test/setup.sh
-go install  ./...
+go install ./...
 nohup python2.7 start.py &
 sleep 2
 cat nohup.out
