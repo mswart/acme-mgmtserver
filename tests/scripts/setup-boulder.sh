63b2e30780c3825ca4e2e652c3ca2acdc3925742
Malte Swart <mswart@devtation.de>
2015-12-14 04:02:23 +0100

    travis: further boulder debugging

--- tests/scripts/setup-boulder.sh
+++ tests/scripts/setup-boulder.sh
@@ -9,7 +9,8 @@ cd $GOPATH/src/github.com/letsencrypt/boulder
 sed -i -e 's/root@tcp/boulder:test@tcp/' policy/_db/dbconf.yml sa/_db/dbconf.yml
 sed -i -e 's/-u root/-u boulder -ptest/' test/create_db.sh
 ./test/setup.sh
-go install ./...
+go install -xv ./...
+python2.7 ./start.py
 nohup python2.7 start.py &
 sleep 2
 cat nohup.out
