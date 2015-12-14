da8ad757d24b410c896ee675e023c14dc6918c4b
Malte Swart <mswart@devtation.de>
2015-12-14 03:42:40 +0100

    travis: boulder source must be inside of GOPATH

--- tests/scripts/setup-boulder.sh
+++ tests/scripts/setup-boulder.sh
@@ -4,13 +4,13 @@ env
 eval "$(gimme 1.5)"
 export GOPATH=~/build/go
 export PATH=$PATH:$GOPATH/bin
-mkdir ~/build/letsencrypt
-git clone git://github.com/letsencrypt/boulder.git --branch release-2015-12-07 ~/build/letsencrypt/boulder
 mkdir -p $GOPATH/src/github.com/letsencrypt/
-ln -s ~/build/letsencrypt/boulder $GOPATH/src/github.com/letsencrypt/boulder
-cd ~/build/letsencrypt/boulder
+git clone git://github.com/letsencrypt/boulder.git --branch release-2015-12-07 $GOPATH/src/github.com/letsencrypt/boulder
+cd $GOPATH/src/github.com/letsencrypt/boulder
 sed -i -e 's/root@tcp/boulder:test@tcp/' policy/_db/dbconf.yml sa/_db/dbconf.yml
 sed -i -e 's/-u root/-u boulder -ptest/' test/create_db.sh
 ./test/setup.sh
+go install  ./...
 nohup python2.7 start.py &
+sleep 2
 cat nohup.out
