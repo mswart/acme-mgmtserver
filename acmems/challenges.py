d6cbb79a5274e6c63353ac86c2fafe943f691f73
Malte Swart <mswart@devtation.de>
2016-03-15 11:58:46 +0100

    WIP implementation for dns-01 challenge implementation

--- acmems/challenges.py
+++ acmems/challenges.py
@@ -1,5 +1,9 @@
 import socket
 from threading import Thread, Event
+from datetime import datetime, timedelta
+import json
+import urllib.request
+import base64
 
 import acme.client
 
@@ -80,8 +84,102 @@ class HttpChallengeImplementor(ChallengeImplementor):
         return content
 
 
+class DnsChallengeImplementor(ChallengeImplementor):
+    """ WIP """
+    def start(self):
+        pass
+
+    def new_authorization(self, authz, client, key, domain):
+        for combination in authz.combinations:
+            if len(combination) == 1:
+                challenger = authz.challenges[combination[0]]
+                challenge = challenger.chall
+                if isinstance(challenge, acme.challenges.DNS):
+                    # store (and deliver) needed response for challenge
+                    content = base64.b64encode(challenge.gen_validation(key).signature.signature)
+
+                    self.add_entry(challenge.validation_domain_name(domain) + '.', content.decode('utf-8'))
+
+                    # answer challenges / give ACME server go to check challenge
+                    resp = challenge.gen_response(key)
+                    client.answer_challenge(challenger, resp)
+
+                    # we can wait until this challenge is first requested ...
+                    raise exceptions.AuthorizationNotYetProcessed(datetime.now() + timedelta(seconds=2))
+        else:
+            return False
+
+
+class DnsChallengeBoulderImplementor(DnsChallengeImplementor):
+    """ WIP """
+    def parse(self, options):
+        self.set_txt_url = None
+        for option, value in options:
+            if option == 'set-txt_url':
+                self.set_txt_url = value
+        if self.set_txt_url is None:
+            self.set_txt_url = 'http://localhost:8055/set-txt'
+
+    def add_entry(self, entry, value):
+        task = json.dumps({
+            'host': entry,
+            'value': value
+        }).encode('utf-8')
+
+        response = urllib.request.urlopen(self.set_txt_url, task)
+        assert response.code is 200
+
+
+class DnsChallengeDnsUpdateImplementor(DnsChallengeImplementor):
+    """ WIP """
+    def parse(self, options):
+        self.dns_servers = None
+        self.ttl = None
+        self.timeout = None
+        for option, value in options:
+            if option == 'dns-server':
+                if self.dns_servers is None:
+                    self.dns_servers = []
+                self.dns_servers.append(value)
+            elif option == 'ttl':
+                self.ttl = int(value)
+            elif option == 'tiemout':
+                self.tiemout = int(value)
+        if self.dns_servers is None:
+            self.dns_servers = ['127.0.0.1']
+        if self.ttl is None:
+            self.ttl = 60
+        if self.timeout is None:
+            self.timeout = 5
+
+    def add_entry(self, entry, value):
+        import dns
+        import dns.update
+        import dns.query
+        upd = dns.update.Update(self.select_zone(entry),
+                                #keyring=dns.tsigkeyring.from_text({keyname: key}),
+                                #keyalgorithm=algo)
+                                )
+        upd.add(entry, self.ttl, 'TXT', value)
+        try:
+            response = dns.query.tcp(upd, self.dns_server, timeout=self.timeout)
+            rcode = response.rcode()
+            if rcode != dns.rcode.NOERROR:
+                rcode_text = dns.rcode.to_text(rcode)
+                raise ValueError(rcode_text)
+            return response
+        except Exception as e:
+            raise ValueError('could not update {}: {}'.format(e.__class__.__name__, e))
+
+    def select_zone(self, entry):
+        parts = entry.split('.')
+        return '.'.join(parts[-3:])
+
+
 implementors = {
-    'http01': HttpChallengeImplementor
+    'http01': HttpChallengeImplementor,
+    'dns01-boulder': DnsChallengeBoulderImplementor,
+    'dns01-dnsUpdate': DnsChallengeDnsUpdateImplementor,
 }
 
 
