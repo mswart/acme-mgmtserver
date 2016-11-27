d5b5f462984d1918e2669f4b684e890bb86a4a08
Malte Swart <mswart@devtation.de>
2016-11-28 00:43:28 +0100

    travis: experiments to get boulder installation back to work

--- tests/scripts/setup-boulder.sh
+++ tests/scripts/setup-boulder.sh
@@ -6,9 +6,9 @@ export PATH=$PATH:$GOPATH/bin
 mkdir -p $GOPATH/src/github.com/letsencrypt/
 git clone git://github.com/letsencrypt/boulder.git --branch release-2016-07-14 $GOPATH/src/github.com/letsencrypt/boulder
 cd $GOPATH/src/github.com/letsencrypt/boulder
-sed -i -e 's/root@tcp/boulder:test@tcp/' policy/_db/dbconf.yml sa/_db/dbconf.yml
-sed -i -e 's/-u root/-u boulder -ptest/' test/create_db.sh
-./test/setup.sh
+#sed -i -e 's/root@tcp/boulder:test@tcp/' policy/_db/dbconf.yml sa/_db/dbconf.yml
+sed -i -e 's/-u root/-u boulder -ptest/' test/create_db.sh || true
+./test/setup.sh || true
 go install -v ./...
 nohup python2.7 start.py &
 sleep 2
