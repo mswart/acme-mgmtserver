56fe1d9b277da4d2ee5c00cbaf39f249dfc57054
Malte Swart <mswart@devtation.de>
2016-11-28 11:49:22 +0100

    more expierments

--- tests/scripts/setup-boulder.sh
+++ tests/scripts/setup-boulder.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-set -ex
+set -x
 eval "$(gimme 1.5)"
 echo "$(gimme 1.5)"
 export GOPATH=~/build/go
-export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOPATH/bin:$GOPATH/src/github.com/letsencrypt/boulder/bin
 mkdir -p $GOPATH/src/github.com/letsencrypt/
 git clone git://github.com/letsencrypt/boulder.git --branch release-2016-07-14 $GOPATH/src/github.com/letsencrypt/boulder
 cd $GOPATH/src/github.com/letsencrypt/boulder
 #sed -i -e 's/root@tcp/boulder:test@tcp/' policy/_db/dbconf.yml sa/_db/dbconf.yml
-sed -i -e 's/-u root/-u boulder -ptest/' test/create_db.sh || true
+sed -i -e 's/-u root/-u boulder -ptest/' test/create_db.sh
 go get -t ./...
 go install -v ./...
-./test/setup.sh || true
+./test/setup.sh
 nohup python2.7 start.py &
 sleep 2
