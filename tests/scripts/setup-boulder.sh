97c3ce55c4fa369807338741e56f2030f5e6eadb
Malte Swart <mswart@devtation.de>
2016-12-01 00:34:51 +0100

    tests: try protobuf from ppa

--- tests/scripts/setup-boulder.sh
+++ tests/scripts/setup-boulder.sh
@@ -8,10 +8,24 @@ git clone git://github.com/letsencrypt/boulder.git --branch hotfixes/2016-06-30 
 cd $GOPATH/src/github.com/letsencrypt/boulder
 #sed -i -e 's/root@tcp/boulder:test@tcp/' policy/_db/dbconf.yml sa/_db/dbconf.yml
 sed -i -e 's/-u root/-u boulder -ptest/' test/create_db.sh
-make
-# we need to install goose now, otherwise the test/create_db.sh call in test/setup.sh will be called before goose is installed
-go get -v bitbucket.org/liamstask/goose/cmd/goose
-./test/setup.sh
+
+go get -v \
+  bitbucket.org/liamstask/goose/cmd/goose \
+  github.com/golang/lint/golint \
+  github.com/golang/mock/mockgen \
+  github.com/golang/protobuf/proto \
+  github.com/golang/protobuf/protoc-gen-go \
+  github.com/jsha/listenbuddy \
+  github.com/kisielk/errcheck \
+  github.com/mattn/goveralls \
+  github.com/modocache/gover \
+  github.com/tools/godep \
+  golang.org/x/tools/cover
+
+go run cmd/rabbitmq-setup/main.go -server amqp://boulder-rabbitmq
+
+./test/create_db.sh
+
 nohup python2.7 start.py &
 sleep 2
 cat nohup.out
