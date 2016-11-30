37926acf2be853d6a18d675ae004fddb35329ea6
Malte Swart <mswart@devtation.de>
2016-12-01 00:16:41 +0100

    travis: try other version (try to avaid panic)
    when starting boulder we often get
    
    E135725 boulder-ca [AUDIT] Panic caused by err: runtime error:
    invalid memory address or nil pointer dereference

--- tests/scripts/setup-boulder.sh
+++ tests/scripts/setup-boulder.sh
@@ -4,7 +4,7 @@ eval "$(gimme 1.6)"
 export GOPATH=~/build/go
 export PATH=$PATH:$GOPATH/bin:$GOPATH/src/github.com/letsencrypt/boulder/bin:~/bin
 mkdir -p $GOPATH/src/github.com/letsencrypt/
-git clone git://github.com/letsencrypt/boulder.git --branch release-2016-07-14 $GOPATH/src/github.com/letsencrypt/boulder
+git clone git://github.com/letsencrypt/boulder.git --branch hotfixes/2016-06-30 $GOPATH/src/github.com/letsencrypt/boulder
 cd $GOPATH/src/github.com/letsencrypt/boulder
 #sed -i -e 's/root@tcp/boulder:test@tcp/' policy/_db/dbconf.yml sa/_db/dbconf.yml
 sed -i -e 's/-u root/-u boulder -ptest/' test/create_db.sh
